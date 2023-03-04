0d861844402bbf85d7a7cbf6f343ecf76717b541
ashjoy7 <ashjoy7@gmail.com>
2023-03-04 04:30:11 -0800

    main.cs for w08

---
+++ prove/Develop04/Main.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<MindfulnessActivity> activities = new List<MindfulnessActivity>();
+        while (true)
+        {
+            MindfulnessActivity.ShowMenu();
+            string userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out int choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        var breathingActivity = new BreathingActivity("Breathing", "Relax with breathing", 5);
+                        breathingActivity.PerformActivity();
+                        activities.Add(breathingActivity);
+                        break;
+                    case 2:
+                        var reflectionActivity = new ReflectionActivity("Reflection", "Reflect on past experiences", 5);
+                        reflectionActivity.PerformActivity();
+                        activities.Add(reflectionActivity);
+                        break;
+                    case 3:
+                        var listingActivity = new ListingActivity("Listing", "List things that make you happy", 5);
+                        listingActivity.PerformActivity();
+                        activities.Add(listingActivity);
+                        break;
+                    case 4:
+                        MindfulnessActivity.ShowLog(activities);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please choose a number between 1 and 4.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please choose a number between 1 and 4.");
+            }
+        }
+    }
+}
